ff2211d36c1a8abc9be54b5e3d6032393a14372d
Justin Quinn <jpq3366@gmail.com>
2023-01-29 23:06:30 -0500

    Set up left side of navbar

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,12 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
-import "@fontsource/open-sans.300.css";
-import "@fontsource/open-sans.400.css";
-import "@fontsource/open-sans.700.css";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
 
 export const theme = extendTheme({
   colors: {},
   brand: {
-    100: "#ff3c00",
+    100: "#FF3C00",
   },
   fonts: {
     body: "Open Sans, sans-serif",
@@ -14,7 +14,7 @@ export const theme = extendTheme({
   styles: {
     global: () => ({
       body: {
-        bg: "gray-200",
+        bg: "gray.200",
       },
     }),
   },
